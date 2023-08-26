58aea909389dc64c473a8a3aa86bbb4b676ff80f
郑泽生 <zhengzesheng@gongpin.com>
2023-08-26 16:21:11 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Cloudreve_flutter
+# cloudreve_flutter
+
+基于Flutter框架开发的Cloudreve客户端，支持Android、iOS平台。
+参考 https://cloudreve.org/
---
+++ android/app/src/main/kotlin/com/example/cloudreve_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cloudreve_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
